0ebe5565d70c10eaa7faccc06cbbc08d461325da
Yoo Hyunseok <brianyhs@naver.com>
2023-07-07 16:26:58 +0900

    feat: users api

--- homework/users/views.py
+++ homework/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+# User requirement: create, delete
+class UserView(APIView):
+     # create user
+    @swagger_auto_schema(request_body=UserSerializer, operation_id="Create user")
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        # if not valid
+        return JsonResponse(serializer.errors, status=404)
+
+    # delete user
+    @swagger_auto_schema(operation_id="Delete user")
+    def delete(self, request, pk):
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return JsonResponse(
+            {"message": "deleted"},
+            status=204
+        )
